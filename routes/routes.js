68af67a35788b02dacb5197b809f7e9d4bf8dac3
alvihossain1 <alvinoorhossain@gmail.com>
2025-04-29 17:23:31 +0600

    add base url

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const { authorizationCheck } = require('../middleware/auth');
 const { postMoviesController, getMoviesController, getMovieByIdController } = require('../controllers/moviesController');
 const router = express.Router();
 
+router.get("/", (req, res) => { res.status(200).json(`Server is running okay and live on this url: ${process.env.SERVER_URL}, Hello there!`) })
 router.post("/auth/register", registerController);
 router.post("/auth/login", loginController);
 router.post("/auth/refresh-token", handleAccessToken)
--- server.js
+++ server.js
@@ -22,14 +22,6 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 
 
-
-//ADDED FOR SEVER RENDERING
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.use('/public', express.static(path.join(__dirname, 'public')))
-
-
-
 const routes = require("./routes/routes");
 const { socketIoConnection } = require("./lib/socketio");
 app.use(routes);
@@ -50,22 +42,6 @@ const io = new Server(server, {
   });
   
   socketIoConnection(io, app)
-
-// app.get('/', (req, res) => {
-//     res.send({ status: 200, data: "Server running okay" });
-// })
-
-// const d = new Date(Date.now())
-// console.log(Date.now())
-
-
-//ADDED FOR SEVER RENDERING
-// if (process.env.NODE_ENV === "production") {
-//     app.get('/*', function (req, res) {
-//         res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//     });
-// }
-
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     server.listen(PORT, () => {
